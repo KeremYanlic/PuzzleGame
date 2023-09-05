f7ffa262ab43d6b7161ea4954c1db9700c3f042a
KeremYanlic <keremyanlc@outlook.com>
2023-09-05 21:39:44 +0300

    LevelManagerUI script has been updated

---
+++ Scripts/LevelManagerUI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using static System.Net.Mime.MediaTypeNames;
+
+public class LevelManagerUI : MonoBehaviour
+{
+    private int levelReached;
+
+    [SerializeField] private List<Button> levelBtns;
+
+    [SerializeField] private Color nonInteractableColor;
+    [SerializeField] private Color interactableColor;
+    private void Start()
+    {
+        levelReached = PlayerPrefs.GetInt("highestLevel", 1);
+
+        for (int i = 0; i < levelBtns.Count; i++)
+        {
+            if (i + 1 > levelReached)
+            {
+                levelBtns[i].interactable = false;
+            }
+        }
+
+        foreach (Button btn in levelBtns)
+        {
+            if (btn.interactable)
+            {
+                btn.gameObject.GetComponent<Image>().color = interactableColor;
+            }
+            else
+            {
+                btn.gameObject.GetComponent<Image>().color = nonInteractableColor;
+            }
+        }
+    }
+
+
+    public void ChooseLevel(int levelIndex)
+    {
+        SceneManager.LoadScene("GameMenu");
+
+        PlayerPrefs.SetInt("currentLevel", levelIndex);
+    }
+}
