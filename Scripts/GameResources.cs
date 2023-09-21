e058f1abc6979676ac0a69dd3b6b64d441224024
KeremYanlic <keremyanlc@outlook.com>
2023-09-22 00:18:17 +0300

    GameResources script file has been updated

---
+++ Scripts/GameResources.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResources : SingletonMonobehaviour<GameResources>
+{
+    public AudioClip clickAudioClip;
+    public AudioClip dingAudioClip;
+    public AudioClip errorAudioClip;
+    public AudioClip winAudioClip;
+    public AudioClip clapAudioClip;
+    public AudioClip whooshAudioClip;
+    public AudioClip btnHoverAudioClip;
+}
